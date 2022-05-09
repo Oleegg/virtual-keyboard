80e7590b903ed71db530d3b82e7e0fb647339ca0
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-05-09 19:19:43 +0300

    feat: add rulles in style.scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log(1);
+let isCaps = false,
+    isLang = false
+const body = document.querySelector('body')
+const wrapper = document.createElement('div')
+console.log(body);
+wrapper.classList.add('wrapper')
+body.append(wrapper)
